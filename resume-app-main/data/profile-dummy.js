feb734bc59ddaf7613238e215ba20550f5242b01
Humble581 <jerryaidoo21@gmail.com>
2024-11-02 03:55:35 +0400

    Add files via upload

---
+++ resume-app-main/data/profile-dummy.js
@@ -0,0 +1,115 @@
+const profileData = {
+  title: 'Resume',
+  name: 'Jeremiah Louis Aidoo',
+  sub_title: 'System Administrator and DevOps Engineer',
+  about: {
+    intro: `Junior Software Engineer with 1 years of FullStack development experience. Expert in building high-performance UI and seamless UX. Experienced in leading projects, migrating legacy systems, and creating reusable components. Skilled in A/B testing, cross-platform compatibility, and responsive design. Effective communicator and collaborator dedicated to continuous learning and innovation..`,
+    contact: {
+      email: 'jerryaidoo21@gmail.com',
+      phone: '+971508453836',
+      address: 'Ghana'
+    }
+  },
+  links: [
+    { title: 'LinkedIn' ,src: 'https://www.linkedin.com/in/jerry-aidoo-4b718b258?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app'},
+    { title: 'Github', src: 'https://github.com/Humble581' }
+  ],
+  education: [
+    {
+      alma: 'Methodist High School',
+      duration: '2012-2015',
+      std: 'High School Diploma',
+      score: '99.99%'
+    },
+    {
+      alma: 'Skynet IT Institute',
+      duration: '2015 - 2020',
+      std: 'Computer Science Basics',
+      score: '99.99%'
+    }
+  ],
+  skills: 
+       [
+    {
+      title: "Operating System",
+      value: "Linux, Windows",
+    },
+    {
+      title: "Languages",
+      value: "Bash, Java,JavaScript,C++ & Python Scripting",
+    },
+    {
+      title: "Version Control",
+      value: "Git, GitHub",
+    },
+    {
+      title: "CI-CD",
+      value: 'Jenkins, GitHub Actions',
+    },
+    {
+      title:' Container & Orchestration',
+      value: 'Docker, Kubernetes',
+    },
+    {
+      title: 'Monitoring & Observability',
+      value: 'Prometheus, Grafana',
+    },
+    {
+      title: 'Cloud Computing',
+      value: 'AWS',
+    },
+    {
+      title:' Infrastructure-as-Code & Configuration Mgt.',
+      value: 'Terraform, Ansible',
+    },
+  ],
+  projects: [
+    {
+      title: 'Lorem Ipsum',
+      duration: 'Mar - Nov 1885',
+      link: 'https://github.com/',
+      desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`
+    },
+    {
+      title: 'Lorem Ipsum',
+      duration: 'Apr - Jul 1917',
+      link: 'https://github.com/',
+      desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`
+    }
+  ],
+  experiences: [
+    {
+      organization: 'World War 2',
+      title: `General`,
+      date: '1941 - 1945',
+      details: [
+        `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
+        `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
+      ]
+    },
+    {
+      organization: 'World War 1',
+      title: `Soldier`,
+      date: '1914 - 1918',
+      details: [
+        `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
+        `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
+      ]
+    }
+  ],
+  certifications: [
+    {
+      desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+      date: 'Aug 1917'
+    },
+    {
+      desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+      date: 'Jul 1945'
+    }
+  ],
+  events: []
+};
